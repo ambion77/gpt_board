ca13755bb8e67c2ae283e7ec2a412b0dd110adb5
ambion77 <ambion77@gmail.com>
2025-02-21 15:54:04 +0900

    개발서버 설정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // 또는 '0.0.0.0'
+    //port: 3000, //5173과 3000번 포트 둘다 사용해야 하므로 주석처리 
+    allowedHosts:['localhost'|| 'ec2-13-124-155-126.ap-northeast-2.compute.amazonaws.com'] // EC2 퍼블릭 도메인 추가
+  }
 })
